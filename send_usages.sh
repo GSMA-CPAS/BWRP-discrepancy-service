0b1999de9dad34ef89a576b100b4743bde4a3df0
ldudzinski <lukasz.dudzinski@ovoo.pl>
2021-03-31 13:40:29 +0200

    Partner usage ID corrected.

--- send_usages.sh
+++ send_usages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -X 'PUT' \
-  'http://127.0.0.1:8080/usages/6056f30bc257e800281964f75f1b?partnerUsageId=6056f30bc257e800281964f75f1b?partnerUsageId=6056f30bc257e800281964f75f1b?partnerUsageId=6056f30bc257e800281964f75f1b' \
+  'http://127.0.0.1:8080/usages/6056f30bc257e800281964f75f1b?partnerUsageId=6056f30bc257e800281964f75f1b' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '[
